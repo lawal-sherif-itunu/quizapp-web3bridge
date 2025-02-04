8aa33c133245c4c5480b8630ed855e4da1d3cacd
Sherif Lawal <lawalsherif0000@gmail.com>
2025-02-04 19:13:50 +0100

    great

--- js/sandbox.js
+++ js/sandbox.js
@@ -6,6 +6,7 @@ const quiz_box = document.querySelector(".quiz_box");
 const option_list = document.querySelector(".option_list");
 const timeCount = quiz_box.querySelector(".timer .timer_sec");
 const timeLine = quiz_box.querySelector("header .time_line");
+const timeoff = quiz_box.querySelector("header .time_text");
 
 // If Start Quiz button is clicked
 start_btn.onclick = () => {
@@ -32,14 +33,38 @@ continue_btn.onclick = () => {
 let que_count = 0;
 let que_numb = 1;
 let counter;
+let counterLine;
 let timeValue = 15;
 let widthvalue = 0;
 let userScore = 0;
 
 const next_btn = document.querySelector(".next_btn");
 const result_box = document.querySelector(".result_box");
-const restart_quiz = result_box.querySelector(".buttons .restart");
-const quit_quiz = result_box.querySelector(".buttons .quit");
+const restart_quiz = result_box.querySelector(".buttons button.restart");
+const quit_quiz = result_box.querySelector(".buttons button.quit");
+
+restart_quiz.onclick = () => {
+  quiz_box.classList.add("activeQuiz");
+  result_box.classList.remove("activeResult");
+  let que_count = 0;
+  let que_numb = 1;
+  let timeValue = 15;
+  let widthvalue = 0;
+  let userScore = 0;
+  showQuestions(que_count);
+  queCounter(1);
+  queCounter(que_numb);
+  clearInterval(counter);
+  startTimer(timeValue);
+  clearInterval(counterLine);
+  startTimerLine(widthvalue);
+  next_btn.style.display = "none";
+  timeoff.textContent = "Time Left";
+};
+
+quit_quiz.onclick = () => {
+  window.location.reload();
+};
 
 // If Next Button is CLicked
 next_btn.onclick = () => {
@@ -54,7 +79,10 @@ next_btn.onclick = () => {
     clearInterval(counterLine);
     startTimerLine(widthvalue);
     next_btn.style.display = "none";
+    timeoff.textContent = "Time Left";
   } else {
+    clearInterval(counter);
+    clearInterval(counterLine);
     console.log("Questions completed");
     showResultBox();
   }
@@ -103,9 +131,9 @@ function optionSelected(answer) {
   let userAns = answer.textContent;
   let correctAns = questions[que_count].answer;
   let allOptions = option_list.children.length;
-  userScore += 1;
-  console.log(userScore);
   if (userAns == correctAns) {
+    userScore += 1;
+    console.log(userScore);
     answer.classList.add("correct");
     console.log("Answer is correct");
     answer.insertAdjacentHTML("beforeend", tickIcon);
@@ -135,7 +163,7 @@ function showResultBox() {
   quiz_box.classList.remove("activeQuiz"); // hide the quiz box
   result_box.classList.add("activeResult"); // show the result box
   const scoreText = result_box.querySelector(".score_text");
-  if (userScore > 3) {
+  if (userScore > 35) {
     let scoreTag =
       "<span>and sorry, You got only<p>" +
       userScore +
@@ -143,7 +171,7 @@ function showResultBox() {
       questions.length +
       "</p></span>";
     scoreText.innerHTML = scoreTag;
-  } else if (userScore > 1) {
+  } else if (userScore > 25) {
     let scoreTag =
       "<span>and congrats!, You got only<p>" +
       userScore +
@@ -174,6 +202,22 @@ function startTimer(time) {
     if (time < 0) {
       clearInterval(counter);
       timeCount.textContent = "00";
+      timeoff.textContent = "Time off";
+
+      let correctAns = questions[que_count].answer;
+      let allOptions = option_list.children.length;
+
+      for (let i = 0; i < allOptions; i++) {
+        if (option_list.children[i].textContent == correctAns) {
+          option_list.children[i].setAttribute("class", "option correct");
+          option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
+        }
+      }
+
+      for (let i = 0; i < allOptions; i++) {
+        option_list.children[i].classList.add("disabled");
+      }
+      next_btn.style.display = "block";
     }
   }
 }
