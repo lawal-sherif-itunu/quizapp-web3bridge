cead5ff6f7a325bb08c135cad95e43348882f541
Sherif Lawal <lawalsherif0000@gmail.com>
2025-02-04 14:24:37 +0100

    js workings

--- js/sandbox.js
+++ js/sandbox.js
@@ -3,6 +3,8 @@ const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
+const option_list = document.querySelector(".option_list");
+const timeCount = quiz_box.querySelector(".timer .timer_sec");
 
 // If Start Quiz button is clicked
 start_btn.onclick = () => {
@@ -20,12 +22,15 @@ continue_btn.onclick = () => {
   quiz_box.classList.add("activeQuiz"); // show the quiz
   showQuestions(0);
   queCounter(1);
+  startTimer(15);
 };
 
 //
 
 let que_count = 0;
 let que_numb = 1;
+let counter;
+let timeValue = 15;
 
 const next_btn = document.querySelector(".next_btn");
 
@@ -45,7 +50,6 @@ next_btn.onclick = () => {
 // getting questions and options from array;
 function showQuestions(index) {
   const que_text = document.querySelector(".que_text");
-  const option_list = document.querySelector(".option_list");
   let que_tag =
     "<span>" +
     questions[index].numb +
@@ -67,6 +71,53 @@ function showQuestions(index) {
     "<span></span></div>";
   que_text.innerHTML = que_tag;
   option_list.innerHTML = option_tag;
+
+  const option = option_list.querySelectorAll(".option");
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
+
+let tickIcon =
+  '<div class="icon tick"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg></div>';
+
+let crossIcon =
+  '<div class="icon cross"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg></div>';
+
+function optionSelected(answer) {
+  let userAns = answer.textContent;
+  let correctAns = questions[que_count].answer;
+  let allOptions = option_list.children.length;
+  if (userAns == correctAns) {
+    answer.classList.add("correct");
+    console.log("Answer is correct");
+    answer.insertAdjacentHTML("beforeend", tickIcon);
+  } else {
+    answer.classList.add("incorrect");
+    console.log("Answer is wrong");
+    answer.insertAdjacentHTML("beforeend", crossIcon);
+
+    // if answers is incorrect then automatically select the correct answer
+    for (let i = 0; i < allOptions; i++) {
+      if (option_list.children[i].textContent == correctAns) {
+        option_list.children[i].setAttribute("class", "option correct");
+        option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
+      }
+    }
+  }
+
+  // once user selected disable all options
+  for (let i = 0; i < allOptions; i++) {
+    option_list.children[i].classList.add("disabled");
+  }
+}
+
+function startTimer(time) {
+  counter = setInterval(timer, 1000);
+  function timer() {
+    timeCount.textContent = time;
+    time--;
+  }
 }
 
 function queCounter(index) {
