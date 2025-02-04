3f7f5421ecce199031c1be9b857fa355d24051e1
Sherif Lawal <lawalsherif0000@gmail.com>
2025-02-04 19:53:26 +0100

    light and dark mode

--- js/sandbox.js
+++ js/sandbox.js
@@ -120,10 +120,10 @@ function showQuestions(index) {
 }
 
 let tickIcon =
-  '<div class="icon tick"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg></div>';
+  '<div class="icon tick"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg></div>';
 
 let crossIcon =
-  '<div class="icon cross"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg></div>';
+  '<div class="icon cross"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></div>';
 
 function optionSelected(answer) {
   clearInterval(counter);
@@ -223,11 +223,11 @@ function startTimer(time) {
 }
 
 function startTimerLine(time) {
-  counterLine = setInterval(timer, 29);
+  counterLine = setInterval(timer, 20);
   function timer() {
     time += 1;
     timeLine.style.width = time + "px";
-    if (time > 549) {
+    if (time > 350) {
       clearInterval(counterLine);
     }
   }
